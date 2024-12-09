7fc6b75438eea26b107ef01b44cf2b8d0ac9f7ed
entervictini <stakakalm@gmail.com>
2024-12-09 00:49:09 -0700

    PartsSub

--- Test/pageobjects/PartsSub.js
+++ Test/pageobjects/PartsSub.js
@@ -5,7 +5,9 @@ import Parts from '../pageobjects/PartsMain.js'
 class PartsSub extends Page {
     async PartsSub () {
     await Parts.Parts();
+    }
 
+    async Advan () {
     await Advan.moveTo();
     const RG4 = await $('//a[@class="m-link" and contains(text(), "RG4")]');
     await RG4.waitForExist({ timeout: 5000});
@@ -82,8 +84,251 @@ class PartsSub extends Page {
                 done();
         }, 600); 
     });
+
+    const BMW = await $('//a[@class="m-link" and contains(text(), "BMW")]');
+    await RSX.waitForExist({ timeout: 5000});
+    await RSX.waitForDisplayed({ timeout: 5000 });
+    expect(RSX).toBeDisplayed();
+    await RSX.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const E36 = await $('//a[@class="m-link" and contains(text(), "E36")]');
+    await RSX.waitForExist({ timeout: 5000});
+    await RSX.waitForDisplayed({ timeout: 5000 });
+    expect(RSX).toBeDisplayed();
+    await RSX.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const M3 = await $('//a[@class="m-link" and contains(text(), "M3")]');
+    await RSX.waitForExist({ timeout: 5000});
+    await RSX.waitForDisplayed({ timeout: 5000 });
+    expect(RSX).toBeDisplayed();
+    await RSX.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const Mazda = await $('//a[@class="m-link" and contains(text(), "Mazda")]');
+    await Mazda.waitForExist({ timeout: 5000});
+    await Mazda.waitForDisplayed({ timeout: 5000 });
+    expect(Mazda).toBeDisplayed();
+    await Mazda.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const Miata = await $('//a[@class="m-link" and contains(text(), "Miata")]');
+    await Miata.waitForExist({ timeout: 5000});
+    await Miata.waitForDisplayed({ timeout: 5000 });
+    expect(Miata).toBeDisplayed();
+    await Miata.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const RX7 = await $('//a[@class="m-link" and contains(text(), "RX7")]');
+    await RX7.waitForExist({ timeout: 5000});
+    await RX7.waitForDisplayed({ timeout: 5000 });
+    expect(RX7).toBeDisplayed();
+    await RX7.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const RX8 = await $('//a[@class="m-link" and contains(text(), "RX8")]');
+    await RX8.waitForExist({ timeout: 5000});
+    await RX8.waitForDisplayed({ timeout: 5000 });
+    expect(RX8).toBeDisplayed();
+    await RX8.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const Nissan = await $('//a[@class="m-link" and contains(text(), "Nissan")]');
+    await Nissan.waitForExist({ timeout: 5000});
+    await Nissan.waitForDisplayed({ timeout: 5000 });
+    expect(Nissan).toBeDisplayed();
+    await Nissan.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const TwoFortySX = await $('//a[@class="m-link" and contains(text(), "240SX")]');
+    await TwoFortySX.waitForExist({ timeout: 5000});
+    await TwoFortySX.waitForDisplayed({ timeout: 5000 });
+    expect(TwoFortySX).toBeDisplayed();
+    await TwoFortySX.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const ThreeHundredZX = await $('//a[@class="m-link" and contains(text(), "300ZX")]');
+    await ThreeHundredZX.waitForExist({ timeout: 5000});
+    await ThreeHundredZX.waitForDisplayed({ timeout: 5000 });
+    expect(ThreeHundredZX).toBeDisplayed();
+    await ThreeHundredZX.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const ThreeFiftyZ = await $('//a[@class="m-link" and contains(text(), "350Z")]');
+    await ThreeFiftyZ.waitForExist({ timeout: 5000});
+    await ThreeFiftyZ.waitForDisplayed({ timeout: 5000 });
+    expect(ThreeFiftyZ).toBeDisplayed();
+    await ThreeFiftyZ.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const Subaru = await $('//a[@class="m-link" and contains(text(), "Subaru")]');
+    await Subaru.waitForExist({ timeout: 5000});
+    await Subaru.waitForDisplayed({ timeout: 5000 });
+    expect(Subaru).toBeDisplayed();
+    await Subaru.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
+
+    const BRZ = await $('//a[@class="m-link" and contains(text(), "BRZ")]');
+    await BRZ.waitForExist({ timeout: 5000});
+    await BRZ.waitForDisplayed({ timeout: 5000 });
+    expect(BRZ).toBeDisplayed();
+    await BRZ.moveTo();
+
+    await browser.executeAsync(async (done) => {
+        setTimeout(() => {
+                done();
+        }, 600); 
+    });
     }
-    
+
+    async ARP () {
+
+    }
+
+    async ChaseBays () {
+
+    }
+
+    async Deatschwerks () {
+
+    }
+
+    async Enkei () {
+
+    }
+
+    async Fifteen52 () {
+
+    }
+
+    async FunkMotorsport () {
+
+    }
+
+    async Garrett () {
+
+    }
+
+    async GramLights () {
+
+    }
+
+    async Greddy () {
+
+    }
+
+    async HKS () {
+
+    }
+
+    async ISRperformance () {
+
+    }
+
+    async Kansei () {
+
+    }
+
+    async KillAllWipers () {
+
+    }
+
+    async KoyoRadiators () {
+
+    }
+
+    async Mishimoto () {
+
+    }
+
+    async MOMO () {
+
+    }
+
+    async NRG () {
+
+    }
+
+    async RbRebuilds () {
+
+    }
+
+    async SeemsLegitGarage () {
+
+    }
+
+    async SPAL () {
+
+    }
+
+    async Sparco () {
+
+    }
+
+    async Turbosmart () {
+        
+    }
     
     open () {
         return super.open();
